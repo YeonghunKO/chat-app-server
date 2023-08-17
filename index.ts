792289931c381dcb191cfedf9915b4c719e0522d
Sinkyo <yhko1988@gmail.com>
2023-08-17 16:37:13 +0900

    fix : / 가 빠져서 404에러

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import AuthRoutes from "./routes/AuthRoutes";
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// api/auth가 base url이고 AuthRoutes안에 지정된 path가 sub path이다
+app.use("/api/auth", AuthRoutes);
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`server is listening to ${process.env.PORT}`);

b83c86696e3b9bdb07c826f574795bf9f8bd72dd
Sinkyo <yhko1988@gmail.com>
2023-08-26 14:04:13 +0900

    fix : cookie 사용
    - set cookie할때 cors 방지

--- index.ts
+++ index.ts
@@ -2,13 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import AuthRoutes from "./routes/AuthRoutes";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 // api/auth가 base url이고 AuthRoutes안에 지정된 path가 sub path이다
 app.use("/api/auth", AuthRoutes);

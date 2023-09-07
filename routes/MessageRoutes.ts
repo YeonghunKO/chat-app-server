a0255b50c702b34917928056b9bf81913b898db7
Sinkyo <yhko1988@gmail.com>
2023-09-07 16:54:01 +0900

    feat : get-messages 라우팅에 추가

--- routes/MessageRoutes.ts
+++ routes/MessageRoutes.ts
@@ -3,7 +3,7 @@ import { addMessage, getMessages } from "../controller/MessageController";
 
 const router = Router();
 
-router.get("/get-messages", getMessages);
+router.get("/get-messages/:from/:to", getMessages);
 router.post("/add-message", addMessage);
 
 export default router;

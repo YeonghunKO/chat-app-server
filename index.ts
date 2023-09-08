15862dfa0ed21be455ef48d4a93c372f2be79d64
Sinkyo <yhko1988@gmail.com>
2023-09-08 15:29:20 +0900

    feat : onlineUsers 모듈로 적용

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
 
 // routes
 import AuthRoutes from "./routes/AuthRoutes";
 import MessageRoutes from "./routes/MessageRoutes";
 
 import errorHandle from "./utils/errorHandle";
+import { onlineUser } from "./utils/onlineUser";
 
 dotenv.config();
 
@@ -32,4 +34,37 @@ app.use("/api/message", MessageRoutes);
 app.use(errorHandle);
 const server = app.listen(process.env.PORT, () => {
   console.log(`server is listening to ${process.env.PORT}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("add-user", (userId: number) => {
+    if (userId) {
+      onlineUser.setUserBySocketId(userId, socket.id);
+    }
+  });
+
+  // client에서 send-msg 보낸것을 서버에서 받은다음 다시 서버에서 클라이언트로 recieve-msg를 전송한다.
+  // 그럼 client에서 recieve-msg를 받는다.
+  socket.on(
+    "send-msg",
+    (data: { to: number; from: number; message: string }) => {
+      const recievedUserLoggedIn = onlineUser.isUserLoggedIn(data.to);
+
+      if (recievedUserLoggedIn) {
+        const socketIdByUserId = onlineUser.getSocketIdByUserId(data.to);
+        socket.to(socketIdByUserId).emit("recieve-msg", {
+          from: data.from,
+          to: data.to,
+          message: data.message,
+        });
+      }
+    }
+  );
 });

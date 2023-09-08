8af838957e3bc63df39e19733350a5ffaf51eea5
Sinkyo <yhko1988@gmail.com>
2023-09-08 15:28:50 +0900

    feat : 로그인한 유저 관리 모듈화
    - onlineUsers 클로져됨

--- utils/onlineUser.ts
+++ utils/onlineUser.ts
@@ -1,1 +1,28 @@
-export const onlineUser = new Map();
+interface IOnlineUser {
+  setUserBySocketId: (userId: number, socketId: string) => void;
+  isUserLoggedIn: (userId: number) => boolean;
+  getSocketIdByUserId: (userId: number) => string;
+}
+
+class OnlineUser implements IOnlineUser {
+  private onlineUser: Map<any, any>;
+  constructor() {
+    this.onlineUser = new Map();
+  }
+
+  isUserLoggedIn(userId: number) {
+    return !!this.onlineUser.get(userId);
+  }
+
+  getSocketIdByUserId(userId: number) {
+    return this.onlineUser.get(userId);
+  }
+
+  setUserBySocketId(userId: number, socketId: string) {
+    this.onlineUser.set(userId, socketId);
+  }
+}
+
+const onlineUser = new OnlineUser();
+
+export { onlineUser };

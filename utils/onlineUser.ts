065f720b5fb8d57ee8332420f39e47893ca7c6d5
Sinkyo <yhko1988@gmail.com>
2023-09-14 17:18:31 +0900

    feat : onlineUsers의 value에 여러가지 상태를 추가.

--- utils/onlineUser.ts
+++ utils/onlineUser.ts
@@ -1,25 +1,69 @@
+export type TOnlineUser = { socketId: string; chatRoomId: number | undefined };
+
 interface IOnlineUser {
-  setUserBySocketId: (userId: number, socketId: string) => void;
+  setSocketIdByUserId: (userId: number, socketId: string) => void;
   isUserLoggedIn: (userId: number) => boolean;
-  getSocketIdByUserId: (userId: number) => string;
+  getSocketIdByUserId: (userId: number) => string | undefined;
+  getChatRoomIdByUserId: (userId: number) => number | undefined;
+  setChatRoomIdByUserId: (userId: number, chatRoomId: number) => void;
+
+  setUserValueById: ({
+    userId,
+    value,
+  }: {
+    userId: number;
+    value: TOnlineUser;
+  }) => void;
 }
 
 class OnlineUser implements IOnlineUser {
-  private onlineUsers: Map<any, any>;
+  private onlineUsers: Map<number, TOnlineUser>;
   constructor() {
     this.onlineUsers = new Map();
   }
 
+  get onlineUsersData() {
+    return this.onlineUsers;
+  }
+
   isUserLoggedIn(userId: number) {
-    return !!this.onlineUsers.get(userId);
+    return this.onlineUsers.has(userId);
   }
 
   getSocketIdByUserId(userId: number) {
-    return this.onlineUsers.get(userId);
+    return this.onlineUsers.get(userId)?.socketId;
+  }
+  getChatRoomIdByUserId(userId: number) {
+    return this.onlineUsers.get(userId)?.chatRoomId;
   }
 
-  setUserBySocketId(userId: number, socketId: string) {
-    this.onlineUsers.set(userId, socketId);
+  setSocketIdByUserId(userId: number, socketId: string) {
+    if (this.onlineUsers.has(userId)) {
+      this.onlineUsers.set(userId, {
+        chatRoomId: this.onlineUsers.get(userId)?.chatRoomId || undefined,
+        socketId,
+      });
+    } else {
+      this.onlineUsers.set(userId, { chatRoomId: 0, socketId });
+    }
+  }
+
+  setChatRoomIdByUserId(userId: number, chatRoomId: number) {
+    if (this.onlineUsers.has(userId)) {
+      this.onlineUsers.set(userId, {
+        chatRoomId,
+        socketId: this.onlineUsers.get(userId)?.socketId || "",
+      });
+    }
+  }
+
+  setUserValueById({ userId, value }: { userId: number; value: TOnlineUser }) {
+    if (this.onlineUsers.get(userId)) {
+      this.onlineUsers.set(userId, {
+        ...this.onlineUsers.get(userId),
+        ...value,
+      });
+    }
   }
 }
 

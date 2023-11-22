41e2070bb87b021daa24773524e841fbc84d2293
Sinkyo <yhko1988@gmail.com>
2023-11-22 14:07:59 +0900

    refactor : 이미 존재하는 email일때 예외처리

--- utils/errorHandle.ts
+++ utils/errorHandle.ts
@@ -8,14 +8,24 @@ const errorHandle = (
   console.error("err", error);
 
   if (error.code) {
-    return res.status(503).json({
-      error,
-      message: `서버에 문제가 발생했습니다. 에러 코드 ${error.code}`,
-    });
+    switch (error.code) {
+      case "P2002":
+        const target = error.meta.target[0];
+        return res.status(503).json({
+          error,
+          message: `이미 존재하는 ${target} 이에요!`,
+        });
+
+      default:
+        return res.status(503).json({
+          error,
+          message: `서버에 문제가 발생했어요. 에러 코드 ${error.code}`,
+        });
+    }
   } else {
     return res.status(503).json({
       error,
-      message: `서버에 문제가 발생했습니다. 서버 로그를 확인해주세요`,
+      message: `서버에 문제가 발생했어요. 서버 로그를 확인해주세요`,
     });
   }
 };

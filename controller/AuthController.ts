da123574b4b49e64fd4eeac73b0f24d3bcfc1173
Sinkyo <yhko1988@gmail.com>
2024-01-23 18:02:26 +0900

    fix : 확실하게 쿠키 도메인 명시!

--- controller/AuthController.ts
+++ controller/AuthController.ts
@@ -164,9 +164,13 @@ export const signIn = async (
       },
     });
 
-    res.cookie(COOKIE.ACCESS_TOKEN, accessToken);
+    res.cookie(COOKIE.ACCESS_TOKEN, accessToken, {
+      domain: process.env.NODE_ENV === "production" ? ".online" : "localhost",
+    });
 
-    res.cookie(COOKIE.REFRESH_IDX, encodeURIComponent(email));
+    res.cookie(COOKIE.REFRESH_IDX, encodeURIComponent(email), {
+      domain: process.env.NODE_ENV === "production" ? ".online" : "localhost",
+    });
 
     return res.status(201).json({
       user: {
@@ -208,9 +212,13 @@ export const onSignUpUser = async (
       },
     });
 
-    res.cookie(COOKIE.ACCESS_TOKEN, accessToken);
+    res.cookie(COOKIE.ACCESS_TOKEN, accessToken, {
+      domain: process.env.NODE_ENV === "production" ? ".online" : "localhost",
+    });
 
-    res.cookie(COOKIE.REFRESH_IDX, encodeURIComponent(email));
+    res.cookie(COOKIE.REFRESH_IDX, encodeURIComponent(email), {
+      domain: process.env.NODE_ENV === "production" ? ".online" : "localhost",
+    });
 
     return res.status(201).json({
       user: {

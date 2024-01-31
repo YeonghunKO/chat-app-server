13cc47bbd708907399cba24bc57e073415e2bcbe
Sinkyo <yhko1988@gmail.com>
2024-01-31 19:46:29 +0900

    refactor : 로그 없앰

--- controller/MessageController.ts
+++ controller/MessageController.ts
@@ -162,8 +162,6 @@ export const sendUpdatedChatListsData = async (
     const userId = parseInt(from);
     const prisma = getPrismaInstance();
 
-    console.log("userId", userId);
-
     const usersWithMessages = await prisma.user.findUnique({
       where: { id: userId },
       include: {

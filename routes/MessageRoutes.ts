8f86bd9864409a2a2d40878c47df10834c56695b
Sinkyo <yhko1988@gmail.com>
2024-01-31 18:25:18 +0900

    refactor : 이미지, 오디오 추가시 마찬가지로 동사는 uri에서 제거

--- routes/MessageRoutes.ts
+++ routes/MessageRoutes.ts
@@ -17,8 +17,8 @@ const uploadAudio = multer({ dest: "uploads/recordings/" });
 
 router.get("/messages/from/:from/to/:to", getMessages);
 router.post("/messages", addMessages);
-router.post("/add-image-message", uploadImage.single("image"), addImageMessage);
-router.post("/add-audio-message", uploadAudio.single("audio"), addAudioMessage);
+router.post("/image", uploadImage.single("image"), addImageMessage);
+router.post("/audio", uploadAudio.single("audio"), addAudioMessage);
 router.get("/get-updated-chat-list/:from", sendUpdatedChatListsData);
 router.get("/filter-message/:from/:message", filterMessages);
 

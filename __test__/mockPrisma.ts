ad659717c2976d6e80f951947dcd051cd29ef03f
Sinkyo <yhko1988@gmail.com>
2024-03-02 22:08:18 +0900

    feat : user의 db를 따로 만듬

--- __test__/mockPrisma.ts
+++ __test__/mockPrisma.ts
@@ -1,12 +1,12 @@
-import { mockedPrismaDB } from "./fixtures/mockedPrismaDB";
+import { mockedPrismaUserDB } from "./fixtures/mockedPrismaDB";
 
 jest.mock("../utils/PrismaClient", () => ({
   __esModule: true,
   default: jest.fn(() => ({
     user: {
       create: async (userData: any) => {
-        mockedPrismaDB.push(userData);
-        return mockedPrismaDB;
+        mockedPrismaUserDB.push(userData);
+        return mockedPrismaUserDB;
       },
     },
   })),

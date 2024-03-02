dd643a19a3c9d290b5bdb62ec6a3ff28a41ea4a0
Sinkyo <yhko1988@gmail.com>
2024-03-02 22:07:57 +0900

    feat : user , message db의 타입을 설정

--- __test__/fixtures/mockedPrismaDB.ts
+++ __test__/fixtures/mockedPrismaDB.ts
@@ -1,1 +1,25 @@
-export const mockedPrismaDB: any[] = [];
+interface IUserInfo {
+  id: number | null;
+  email: string | null;
+  name: string | null;
+  profilePicture: string | null;
+  about?: string;
+  password: string;
+  refreshToken: string;
+  sentMessage: IMessages[];
+  recievedMessage: IMessages[];
+}
+
+interface IMessages {
+  id: number;
+  sender: IUserInfo;
+  senderId: number;
+  reciever: IUserInfo;
+  recieverId: number;
+  message: string;
+  type: string;
+  status: string;
+  createdAt: string;
+}
+
+export const mockedPrismaUserDB: IUserInfo[] = [];

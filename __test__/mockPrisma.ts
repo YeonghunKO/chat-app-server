bd35ff6e83a9a300cd6d3bf0675c531df34c4337
Sinkyo <yhko1988@gmail.com>
2024-03-04 12:52:36 +0900

    feat : sign up과정에서 이미 등록된 유저일경우 에러 던지기

--- __test__/mockPrisma.ts
+++ __test__/mockPrisma.ts
@@ -14,7 +14,13 @@ jest.mock("../utils/PrismaClient", () => ({
           },
         } = userData;
 
-        console.log("userData", userData);
+        const isMockedNewUserRegistered = mockedPrismaUserDB.find(
+          (user) => user.email === userInfo.email
+        );
+
+        if (isMockedNewUserRegistered) {
+          throw new Error("User already registered");
+        }
 
         mockedPrismaUserDB.push({
           ...userInfo,
